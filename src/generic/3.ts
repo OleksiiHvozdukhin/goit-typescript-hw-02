217b1780a170ce16548948ddbee5e62591764743
Alexey Hvozduhin <124602564+OleksiiHvozdukhin@users.noreply.github.com>
2023-10-28 12:57:46 +0300

    Update

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,12 +3,8 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, U>(objA: T, objB: U): T&U {
-  return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign(objA, objB);
 }
 
-// const objA = { a: 1, b: 2 };
-// const objB = { c: 3, d: 4 };
-// const merged = merge(objA, objB);
-
-export {}
+export {};
